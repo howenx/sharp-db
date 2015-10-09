8c733a92a50a7a8f58f771fc6208340b9ce2871e
howenkakao <howen.xiong@daumkakao.com>
2015-10-09 16:51:57 +0800

    delete

--- Worksheet 1.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE hp_content
-(
-   id               bigserial           not null,
-   content_html     text,
-   tmp_id           bigint,
-   publish_date     timestamp (5) WITHOUT TIME ZONE,
-   client_flag      CHARACTER (2) DEFAULT '0'::bpchar,
-   html_image_url   CHARACTER VARYING (500)
-)
